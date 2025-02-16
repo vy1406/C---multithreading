847f54d7df83989b0fc594a464d22be69ec6d09a
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-16 14:19:00 -0700

    demonstrating shared resource problem

---
+++ syncOverview/Program.cs
@@ -0,0 +1,21 @@
+﻿int counter = 0;
+
+Thread thread1 = new Thread(IncrementCounter);
+Thread thread2 = new Thread(IncrementCounter);
+thread1.Start();
+thread2.Start();
+
+
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine($"Final counter value is: {counter}");
+
+
+void IncrementCounter()
+{
+    for (int i = 0; i < 100000; i++)
+    {
+        counter = counter + 1;
+    }
+}
