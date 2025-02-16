5052c932cd046fa402277879b1f232b80d7090a3
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-16 12:02:59 -0700

    init threads

---
+++ createThreads/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+void WriteThreadId()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine(Thread.CurrentThread.Name);
+        Thread.Sleep(50);
+    }
+}
+
+
+Thread thread1 = new Thread(WriteThreadId);
+Thread thread2 = new Thread(WriteThreadId);
+
+thread1.Name = "Thread1";
+thread2.Name = "Thread2";
+Thread.CurrentThread.Name = "Main thread";
+
+thread1.Priority = ThreadPriority.Highest;
+thread2.Priority = ThreadPriority.Lowest;
+Thread.CurrentThread.Priority = ThreadPriority.Normal;
+
+thread1.Start();
+thread2.Start();
+WriteThreadId();
+
+Console.ReadLine();
