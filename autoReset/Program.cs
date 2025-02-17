5df7f9b10abfbd7b1525e0bd2019b8c14a539a92
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-17 12:06:41 -0700

    autoResetEvent

---
+++ autoReset/Program.cs
@@ -0,0 +1,83 @@
+﻿///////////////////////////////////////////////////////////////////////
+// Single Worker thread
+/////////////////////////////////////////////////////////////////////// 
+using AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+string? userInput = null;
+
+Console.WriteLine("Server is running. Type 'go' to proceed and  'exit' to stop.");
+
+// Start the worker thread
+for (int i = 0; i < 3; i++)
+{
+    Thread workerThread = new Thread(Worker);
+    workerThread.Name = $"Worker {i + 1}";
+    workerThread.Start();
+}
+
+// Main thread receives user input
+
+while (true)
+{
+    userInput = Console.ReadLine();
+
+    // Signal the worker thread if input is "go"
+    if (userInput.ToLower() == "go")
+    {
+        autoResetEvent.Set();
+    }
+}
+
+
+void Worker()
+{
+    while (true)
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name} is waiting for signal.");
+        // Wait for the signal from the main thread
+        autoResetEvent.WaitOne();
+
+        Console.WriteLine($"{Thread.CurrentThread.Name} proceeds.");
+        // Simulate processing time
+        Thread.Sleep(2000);
+    }
+}
+
+///////////////////////////////////////////////
+// Multiple Worker threads
+//////////////////////////////////////////////
+using AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+string? userInput = null;
+
+
+// Start the worker thread
+Thread workerThread = new Thread(Worker);
+workerThread.Start();
+
+// Main thread receives user input
+Console.WriteLine("Server is running. Type 'exit' to stop.");
+while (true)
+{
+    userInput = Console.ReadLine();
+
+    // Signal the worker thread if input is "go"
+    if (userInput.ToLower() == "go")
+    {
+        autoResetEvent.Set();
+    }
+}
+
+
+void Worker()
+{
+    while (true)
+    {
+        Console.WriteLine("Worker thread is waiting for signal.");
+        // Wait for the signal from the main thread
+        autoResetEvent.WaitOne();
+
+        Console.WriteLine("Worker thread proceeds.");
+        // Simulate processing time
+        Thread.Sleep(2000);
+    }
+}
+
---
+++ semaphore/Program.cs
@@ -0,0 +1,60 @@
+﻿Queue<string?> requestQueue = new Queue<string?>();
+
+object queueLock = new object();
+using SemaphoreSlim semaphore = new SemaphoreSlim(initialCount: 3, maxCount: 3);
+
+// 2. Start the requests queue monitoring thread
+Thread monitoringThread = new Thread(MonitorQueue);
+monitoringThread.Start();
+
+// 1. Enqueue the requests
+Console.WriteLine("Server is running. Type 'exit' to stop.");
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (input?.ToLower() == "exit")
+    {
+        break;
+    }
+
+    lock (queueLock)
+    {
+        requestQueue.Enqueue(input);
+    }
+
+}
+
+void MonitorQueue()
+{
+    while (true)
+    {
+        if (requestQueue.Count > 0)
+        {
+            string? input;
+            lock (queueLock)
+            {
+                input = requestQueue.Dequeue();
+            }
+            semaphore.Wait();
+            Thread processingThread = new Thread(() => ProcessInput(input));
+            processingThread.Start();
+        }
+        Thread.Sleep(100);
+    }
+}
+
+// 3. Processing the requests
+void ProcessInput(string? input)
+{
+    try
+    {
+        // Simulate processing time
+        Thread.Sleep(2000);
+        Console.WriteLine($"Processed input: {input}");
+    }
+    finally
+    {
+        var prevCount = semaphore.Release();
+        Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} released the semaphore. Previous count is: {prevCount}");
+    }
+}
