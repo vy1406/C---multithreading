832eb5488e1d273b835178ffab2305951c8e38aa
Vladimir Elisavetski <yvova88@yahoo.com>
2025-02-18 15:18:26 -0700

    tasks results

---
+++ resultsFromTasks/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+int SumSegment(int start, int end)
+{
+    int segmentSum = 0;
+    for (int i = start; i < end; i++)
+    {
+        Thread.Sleep(100);
+        segmentSum += array[i];
+    }
+
+    return segmentSum;
+}
+
+
+
+
+var startTime = DateTime.Now;
+
+int numofThreads = 4;
+int segmentLength = array.Length / numofThreads;
+
+Task<int>[] tasks = new Task<int>[numofThreads];
+tasks[0] = Task.Run(() => { return SumSegment(0, segmentLength); });
+tasks[1] = Task.Run(() => { return SumSegment(segmentLength, 2 * segmentLength); });
+tasks[2] = Task.Run(() => { return SumSegment(2 * segmentLength, 3 * segmentLength); });
+tasks[3] = Task.Run(() => { return SumSegment(3 * segmentLength, array.Length); });
+
+Console.WriteLine($"The sum is {tasks.Sum(t => t.Result)}");
+
+var endTime = DateTime.Now;
+var timespan = endTime - startTime;
+Console.WriteLine($"The time it takes: {timespan.TotalMilliseconds}");
+
+Console.ReadLine();
